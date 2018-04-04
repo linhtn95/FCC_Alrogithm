efb89509051df9d95ba7ba9784ec8545d92dbffa
tranlinh <trannhatlinh9d@gmail.com>
2018-04-04 13:09:15 +0700

    diff 2 array

---
+++ DiffTwoArray/DiffTwoArray.js
@@ -0,0 +1,17 @@
+// Diff to array
+
+function diffArray(arr1, arr2) {
+    let newArr = [];
+   
+    let a1 = arr1.filter( el => {
+        return arr2.indexOf(el) === -1;
+    });
+    let a2 = arr2.filter( el => {
+        return arr1.indexOf(el) === -1;
+    });
+
+    newArr = a1.concat(a2);
+    return newArr;
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5])
